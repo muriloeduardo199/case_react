5de08316b2707a3a3dde6b187387e51aaeac787b
muriloeduardo199 <muriloeduardo1993@gmail.com>
2022-10-27 18:08:48 -0300

    update

---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "/home/murilo/case/src/App.css"
+
+
+const Navbar = () => {
+    return(
+        <div className="navbar">
+            <h1>shopping app</h1>
+            
+        </div>
+    )
+}
+
+export default Navbar
